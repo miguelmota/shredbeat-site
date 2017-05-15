0de9221a7b8d937e99aaabdab55a378490134935
Miguel Mota <miguelmota2@gmail.com>
2017-05-15 16:24:32 -0700

    preload gifs + add desc

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,10 +1,18 @@
 (function() {
+  var TOTAL_IMAGES = 16
+
+  // preload gifs
+  for (var i = 1; i < TOTAL_IMAGES; i++) {
+    var img = new Image()
+    img.src = '../assets/images/gifs/' + i + '.gif'
+  }
+
   function getRand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
   function changeBg() {
-    document.body.style.backgroundImage = 'url(../assets/images/gifs/' + getRand(1, 16) + '.gif)'
+    document.body.style.backgroundImage = 'url(../assets/images/gifs/' + getRand(1, TOTAL_IMAGES) + '.gif)'
 
     setTimeout(function() {
       changeBg()
